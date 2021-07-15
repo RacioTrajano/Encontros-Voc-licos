6c22a1a2b0100c3543ea4fa469dc9a82a85e843c
racio <85686582+RacioTrajano@users.noreply.github.com>
2021-07-15 14:24:04 +0000

    alteraçao

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #define TAM_TEXTO 100
 int main () {
 
-int acha_pedaco;
+
 int acha_encontro =0;
 char texto[TAM_TEXTO] ={0};
 char encontro[25][2] ={"aa","ae","ai","ao", "au","ea","ee","ei","eo","eu","ia","ie","ii","io","iu","oa","oe","oi","oo","ou","ua","ue","ui","uo","uu"};
